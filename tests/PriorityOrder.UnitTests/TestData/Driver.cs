abbf54b448de51d9b8924c66db97250b75bedc6f
kurnakovv <maksk210@gmail.com>
2024-02-16 22:46:58 +0900

    #9 Add PriorityEnumerable.OrderByPriority(IEnumerable)

---
+++ tests/PriorityOrder.UnitTests/TestData/Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace PriorityOrder.UnitTests.TestData
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string CategoryString { get; set; }
+        public DriverCategory CategoryEnum { get; set; }
+        public int CategoryInt { get; set; }
+    }
+}
---
+++ tests/PriorityOrder.UnitTests/TestData/DriverCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace PriorityOrder.UnitTests.TestData
+{
+    /// <summary>
+    /// Enum with random order.
+    /// </summary>
+    public enum DriverCategory
+    {
+        FirstCategory = 100, 
+        ThirdCategory = 2124,
+        SecondCategory = -12434,
+        None = 0,
+    }
+}
